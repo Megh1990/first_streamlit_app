135d255a523bdc082adda85fba7a714eed92b43b
Megh1990 <102602701+Megh1990@users.noreply.github.com>
2023-01-10 21:17:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title("Hello World")
 streamlit.title("okay bye ")
-:
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
