f314a276297968b12ac124340a68a7a1e822618c
Megh1990 <102602701+Megh1990@users.noreply.github.com>
2023-01-11 18:00:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import snowflake.connector
 streamlit.title("Hello World")
 streamlit.title("okay bye ")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
